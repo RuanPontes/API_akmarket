a53f0a5ec0f7963604ec88d6480a621dd820fc09
sergioricardojsj <sergioricardojsj@gmail.com>
2023-12-16 17:18:47 -0300

    removing id from users validation

--- src/main/java/com/example/testegpt/controller/ValidateUserController.java
+++ src/main/java/com/example/testegpt/controller/ValidateUserController.java
@@ -1,11 +1,15 @@
 package com.example.testegpt.controller;
 
+import static com.example.testegpt.constants.Headers.AUTHORIZATION;
+
 import com.example.testegpt.controller.request.UserValidationRequest;
 import com.example.testegpt.controller.response.UserValidationResponse;
+import com.example.testegpt.service.TokenService;
+import com.example.testegpt.service.UserService;
 import com.example.testegpt.service.ValidationCodeService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class ValidateUserController {
 
   private final ValidationCodeService validationCodeService;
+  private final UserService userService;
+  private final TokenService tokenService;
 
-  public ValidateUserController(ValidationCodeService validationCodeService) {
+  public ValidateUserController(
+      ValidationCodeService validationCodeService,
+      UserService userService,
+      TokenService tokenService) {
     this.validationCodeService = validationCodeService;
+    this.userService = userService;
+    this.tokenService = tokenService;
   }
 
-  @PostMapping("/{id}/validate")
+  @PostMapping("/validate")
   public ResponseEntity<UserValidationResponse> validate(
-      @PathVariable Long id, @RequestBody @Valid UserValidationRequest request) {
-    validationCodeService.validate(id, Integer.parseInt(request.codigo()));
+      @RequestBody @Valid UserValidationRequest request, HttpServletRequest servletRequest) {
+    validationCodeService.validate(getUserId(servletRequest), Integer.parseInt(request.codigo()));
 
     return ResponseEntity.ok(new UserValidationResponse("Código validado com sucesso!"));
   }
 
-  @PostMapping("/{id}/resend")
-  public ResponseEntity<UserValidationResponse> resend(@PathVariable Long id) {
-    validationCodeService.resend(id);
+  @PostMapping("/resend")
+  public ResponseEntity<UserValidationResponse> resend(HttpServletRequest request) {
+    validationCodeService.resend(getUserId(request));
     return ResponseEntity.ok(new UserValidationResponse("O código foi reenviado"));
   }
 
+  private Long getUserId(HttpServletRequest request) {
+    String subject = tokenService.getSubject(request.getHeader(AUTHORIZATION));
+    return userService.findByUsuario(subject).getId();
+  }
 }
